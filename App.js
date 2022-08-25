e0e1cad6da4394d725a21d1b6af248a4cc1fcd4e
can <laksika.0175705@gmail.com>
2022-08-25 20:42:43 +0700

    m

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from './component/Weather';
+export default function App() {
+    const doIt = () => {
+        console.log("Hello from console")
+    }
+    return (
+        <View style={styles.container}>
+            <Weather zipCode="90110" />
+            <StatusBar style="auto" />
+        </View>
+
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
